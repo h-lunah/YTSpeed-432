5b65c88ec74a8c9aeccbaf31126dcebc5463ac4a
luna <71488561+h-lunah@users.noreply.github.com>
2025-02-04 17:47:09 +0100

    proper JS conventions

--- YTSpeed.js
+++ YTSpeed.js
@@ -1,76 +1,64 @@
-﻿let ytSpeed;
+const ytSpeed = {
+    playbackRate: 1.0,
+    preservesPitch: false,
+    init: function () {
+        const observer = new MutationObserver(function (mutations) {
+            ytSpeed.updateVideos();
+        });
 
-if (typeof ytSpeed === 'undefined')
-{
-    ytSpeed =
-        {
-            playbackRate: 1.0,
-            preservesPitch: false,
-            init: function ()
+        observer.observe(
+            document.querySelector('body'),
             {
-                var observer = new MutationObserver(function (mutations)
-                {
-                    ytSpeed.updateVideos();
-                });
-
-                observer.observe(
-                    document.querySelector('body'),
-                    {
-                        attributes: true,
-                        childList: true,
-                        characterData: true,
-                        subtree: true
-                    });
-
-                ytSpeed.updateVideos();
-            },
-            updateVideos: function ()
-            {
-                var videos = document.getElementsByTagName('video');
-                for (var i = 0; i < videos.length; ++i)
-                {
-                    var v = videos[i];
-                    v.playbackRate = this.playbackRate;
-                    v.preservesPitch = v.mozPreservesPitch = this.preservesPitch && this.playbackRate != 1.0;
-                }
-            },
-            speedUp: function ()
-            {
-                this.playbackRate *= Math.pow(2, 1/12);
-                ytSpeed.updateVideos();
-            },
-            speedDown: function ()
-            {
-                this.playbackRate /= Math.pow(2, 1/12);
-                ytSpeed.updateVideos();
-            },
-            hz432: function ()
-            {
-                this.playbackRate *= Math.pow(2, -30/1200);
-                ytSpeed.updateVideos();
-            },
-            hz440: function ()
-            {
-                this.playbackRate /= Math.pow(2, -30/1200);
-                ytSpeed.updateVideos();
-            },
-            reset: function ()
-            {
-                this.playbackRate = 1.0;
-                ytSpeed.updateVideos();
-            },
-            prompt: function ()
-            {
-                var playbackRate = prompt("New playback speed:", this.playbackRate);
-
-                if (!playbackRate)
-                    return;
-                else if (isNaN(parseFloat(playbackRate)))
-                    return;
-
-                this.playbackRate = parseFloat(playbackRate);
-                ytSpeed.updateVideos();
+                attributes: true,
+                childList: true,
+                characterData: true,
+                subtree: true
             }
-        };
+        );
+
+        ytSpeed.updateVideos();
+    },
+    updateVideos: function () {
+        const videos = document.getElementsByTagName('video');
+        for (let i = 0; i < videos.length; ++i) {
+            const v = videos[i];
+            v.playbackRate = this.playbackRate;
+            v.preservesPitch = v.mozPreservesPitch = this.preservesPitch && this.playbackRate !== 1.0;
+        }
+    },
+    speedUp: function () {
+        this.playbackRate *= Math.pow(2, 1/12);
+        ytSpeed.updateVideos();
+    },
+    speedDown: function () {
+        this.playbackRate /= Math.pow(2, 1/12);
+        ytSpeed.updateVideos();
+    },
+    hz432: function () {
+        this.playbackRate *= Math.pow(2, -30/1200);
+        ytSpeed.updateVideos();
+    },
+    hz440: function () {
+        this.playbackRate /= Math.pow(2, -30/1200);
+        ytSpeed.updateVideos();
+    },
+    reset: function () {
+        this.playbackRate = 1.0;
+        ytSpeed.updateVideos();
+    },
+    prompt: function () {
+        const playbackRate = prompt("New playback speed:", this.playbackRate);
+
+        if (!playbackRate)
+            return;
+        else if (isNaN(parseFloat(playbackRate)))
+            return;
+
+        this.playbackRate = parseFloat(playbackRate);
+        ytSpeed.updateVideos();
+    }
+};
+
+if (typeof ytSpeed === 'undefined') {
     ytSpeed.init();
 }
